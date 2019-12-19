5bbd0ac7e632c0514c64eaa2d235c5a44da433d6
papan01 <navy9051@gmail.com>
2019-12-19 17:25:02 +0800

    add netlify.toml, netlify-cms-media-library-uploadcare and netlify-cms-media-library-cloudinary

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,10 @@
 import CMS from 'netlify-cms-app';
 import '../layout/style/style.scss';
 import '../templates/post.scss';
+import uploadcare from 'netlify-cms-media-library-uploadcare';
+import cloudinary from 'netlify-cms-media-library-cloudinary';
 import PostPreview from './preview-templates/postPreview';
 
+CMS.registerMediaLibrary('uploadcare', uploadcare);
+CMS.registerMediaLibrary('cloudinary', cloudinary);
 CMS.registerPreviewTemplate('blog', PostPreview);
