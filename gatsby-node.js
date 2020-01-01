ab450f2949cfd5d7d88a139e5b5400ec0877edb7
papan01 <navy90517@gmail.com>
2020-01-01 20:25:34 +0800

    Fixed prev and next links in post

--- gatsby-node.js
+++ gatsby-node.js
@@ -46,8 +46,8 @@ exports.createPages = async ({ graphql, actions }) => {
   const posts = result.data.allMarkdownRemark.edges;
 
   posts.forEach(({ node }, index) => {
-    const prev = index === 0 ? null : posts[index - 1].node;
-    const next = index === posts.length - 1 ? null : posts[index + 1].node;
+    const next = index === 0 ? null : posts[index - 1].node;
+    const prev = index === posts.length - 1 ? null : posts[index + 1].node;
     createPage({
       path: node.fields.slug,
       component: path.resolve(`./src/templates/post.jsx`),
