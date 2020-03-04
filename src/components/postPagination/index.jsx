ac46bcace0ac8760f9cdfe6223ed5c273d8b11e1
papan01 <navy90517@gmail.com>
2020-03-04 20:29:39 +0800

    Bug fixed: missing forward slash

--- src/components/postPagination/index.jsx
+++ src/components/postPagination/index.jsx
@@ -30,8 +30,8 @@ function pagination(currentPage, pageCount, delta = 2) {
 const PostPagination = ({ currentPage, numPages, subpath }) => {
   const isFirst = currentPage === 1;
   const isLast = currentPage === numPages;
-  const prevPage = currentPage - 1 === 1 ? '/' : (currentPage - 1).toString();
-  const nextPage = (currentPage + 1).toString();
+  const prevPage = currentPage - 1 === 1 ? '/' : `/${(currentPage - 1).toString()}`;
+  const nextPage = `/${(currentPage + 1).toString()}`;
   const pages = pagination(currentPage, numPages);
   return (
     <div className="post-pagination">
